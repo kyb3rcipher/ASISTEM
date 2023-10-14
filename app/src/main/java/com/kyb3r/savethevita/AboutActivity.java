33c46d4a34fc6e81813d86a586898aaa2d687368
Kyb3r Cipher <me@kyb3rcipher.com>
2023-10-14 13:19:27 -0600

    Complete about

--- app/src/main/java/com/kyb3r/savethevita/AboutActivity.java
+++ app/src/main/java/com/kyb3r/savethevita/AboutActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -14,6 +16,11 @@ public class AboutActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarAbout);
         toolbar.setNavigationOnClickListener(v -> onBackPressed());
+
+        // Active contact hyperlink
+        TextView developmentContact = (TextView) findViewById(R.id.developmentContact), designContact = (TextView) findViewById(R.id.designContact);
+        developmentContact.setMovementMethod(LinkMovementMethod.getInstance());
+        designContact.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 }
