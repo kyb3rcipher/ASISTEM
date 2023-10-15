c8ce5c6d3587664a878486611bc31dc0032e19b2
Kyb3r Cipher <me@kyb3rcipher.com>
2023-10-14 20:02:36 -0600

    Add new logo and pulse animation

--- app/src/main/java/com/kyb3r/savethevita/AboutActivity.java
+++ app/src/main/java/com/kyb3r/savethevita/AboutActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
@@ -16,6 +19,12 @@ public class AboutActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarAbout);
         toolbar.setNavigationOnClickListener(v -> onBackPressed());
+
+
+        // Start logo heart-pulse
+        ImageView appLogo = findViewById(R.id.appLogo);
+        Animation pulseAnimation = AnimationUtils.loadAnimation(this, R.anim.pulse_animation);
+        appLogo.startAnimation(pulseAnimation);
 
         // Active contact hyperlink
         TextView developmentContact = (TextView) findViewById(R.id.developmentContact), designContact = (TextView) findViewById(R.id.designContact);
