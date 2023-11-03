0ea6ebcf44dea84ee626b08f3ef7fb5d6fdecfd6
Kyb3r Cipher <me@kyb3rcipher.com>
2023-11-03 13:13:41 -0600

    Create FracturesCourseActivity

---
+++ app/src/main/java/com/kyb3r/asistem/FracturesCourseActivity.java
@@ -0,0 +1,14 @@
+package com.kyb3r.asistem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FracturesCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fractures_course);
+    }
+}
