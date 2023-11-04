668790756235fc2e439e02ff21afe34f71e9439b
Kyb3r Cipher <me@kyb3rcipher.com>
2023-11-04 15:24:53 -0600

    Start example Fractures exercise options

--- app/src/main/java/com/kyb3r/asistem/FracturesCourseActivity.java
+++ app/src/main/java/com/kyb3r/asistem/FracturesCourseActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class FracturesCourseActivity extends AppCompatActivity {
 
-    private int currentFragment = 0;
+    private int nextFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,42 +24,62 @@ public class FracturesCourseActivity extends AppCompatActivity {
         FrameLayout frameLayout = findViewById(R.id.frameLayout);
         getLayoutInflater().inflate(R.layout.exercise_read, frameLayout, true);
         button.setText("Okay");
+        TextView text = frameLayout.findViewById(R.id.text);
+        text.setText(getString(R.string.courseFracturesRead));
+        nextFragment = 0;
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        button.setOnClickListener(v -> {
 
+            switch (nextFragment) {
+                case 0:
+                    changeLayout(R.layout.exercise_options);
+                    button.setText("Check");
 
-
-                switch (currentFragment) {
-                    case 0:
-                        changeLayout(R.layout.exercise_options);
-                        button.setText("Check");
-
-                        TextView instruction = findViewById(R.id.instruction);
-                        instruction.setText("UwU");
-
-                        currentFragment++;
-                    break;
-
-                    case 1:
-                        changeLayout(R.layout.exercise_select);
-                        button.setText("Check");
-
-                        currentFragment++;
-                    break;
-
-                    case 2:
-                        changeLayout(R.layout.exercise_write_answer);
-                        button.setText("Check");
-
-                        currentFragment++;
-                    break;
-                }
+                    TextView instruction = findViewById(R.id.instruction);
+                    instruction.setText("What are fractures?");
 
 
+                    RadioButton option1 = findViewById(R.id.option1), option2 = findViewById(R.id.option2), option3 = findViewById(R.id.option3);
 
+                    option1.setText("NADIE SABE...");
+                    option2.setText("They are breaks in the bones and can vary in severity");
+                    option3.setText("Bad Bunny");
+
+                    RadioGroup radioButton = findViewById(R.id.optionsGroup);
+                    radioButton.setOnCheckedChangeListener((group, checkedId) -> {
+                        nextFragment = (checkedId == R.id.option2 && checkedId == group.getCheckedRadioButtonId()) ? 1 : 0;
+                    });
+                    /*radioButton.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(RadioGroup group, int checkedId) {
+                            if (checkedId == R.id.option2) {
+                                if (group.getCheckedRadioButtonId() == R.id.option2) {
+                                    nextFragment = 1;
+                                }
+                            } else if (checkedId == R.id.option1 || checkedId == R.id.option3) {
+                                if (group.getCheckedRadioButtonId() == R.id.option1 || group.getCheckedRadioButtonId() == R.id.option3) {
+                                    nextFragment = 0;
+                                }
+                            }
+                        }
+                    });*/
+                break;
+
+                case 1:
+                    changeLayout(R.layout.exercise_select);
+                    button.setText("Check");
+
+                    nextFragment++;
+                break;
+
+                case 2:
+                    changeLayout(R.layout.exercise_write_answer);
+                    button.setText("Check");
+
+                    nextFragment++;
+                break;
             }
+
         });
     }
 
