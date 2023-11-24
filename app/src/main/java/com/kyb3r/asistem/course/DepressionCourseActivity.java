a70ae4bd0a25ae37ea37379b8ee38689dd8596ec
Kyb3r Cipher <me@kyb3rcipher.com>
2023-11-23 19:35:02 -0600

    Complete DepressionCourse first progress (1/3)

--- app/src/main/java/com/kyb3r/asistem/course/DepressionCourseActivity.java
+++ app/src/main/java/com/kyb3r/asistem/course/DepressionCourseActivity.java
@@ -37,7 +37,7 @@ public class DepressionCourseActivity extends AppCompatActivity {
                                     "Selecciona sintomas de la depresion:",
                                     "Dolor de cabeza, Llanto, Cambio en la manera de vestirse",
                                     "Apatía, Perdida de interés, Cambios en el apetito, Sentirse inultil/culpable",
-                                    "HOLA",
+                                    "Estar solo todo el dia, dormir mucho, no jugar videojuegos",
                                     R.id.option2,
                                     1, 0, false
                             );
@@ -71,36 +71,87 @@ public class DepressionCourseActivity extends AppCompatActivity {
                                     "6 meses",
                                     "10 años",
                                     R.id.option3,
-                                    4, 3, true
+                                    4, 3, false
                             );
                             break;
                         case 4:
+                            exercises.options(
+                                    "¿Qué diferencia hay entre la depresión clínica y simplemente sentirse deprimido?",
+                                    "La depresión clínica es una respuesta temporal a situaciones como obtener malas calificaciones. En contraste, sentirse deprimido es una respuesta y afeccion medica que puede permsistir durante semanas",
+                                    "La depresión clínica es una afección médica que persiste durante al menos dos semanas y afecta significativamente la capacidad de trabajar, jugar o amar. En contraste, sentirse deprimido puede ser una respuesta temporal a situaciones como obtener malas calificaciones",
+                                    "La depresión clínica es una enfermedad que puede ser detectada por rayos X",
+                                    R.id.option2,
+                                    5, 4, false
+                            );
                             break;
                         case 5:
+                            exercises.options(
+                                    "¿Cuáles son algunos tratamientos efectivos para la depresión?",
+                                    "Terapia, Medicamentos, Tratamientos experimentales como la estimulación magnética transcraneal.",
+                                    "Terapia, Comer mas, Salir con amigos",
+                                    "Terapia, Salir de viaje, Tratamientos experimentales como las drogas",
+                                    R.id.option1,
+                                    6, 5, false
+                            );
                             break;
                         case 6:
+                            exercises.write(
+                                    "Escribe ¿Cómo se describe la terapia electroconvulsiva y en qué casos se menciona que puede ser especialmente útil como tratamiento para la depresión?",
+                                    Arrays.asList("terapia electroconvulsiva", "electroconvulsiva terapia", "electroconvulsiva"),
+                                    7, 6, false
+                            );
                             break;
                         case 7:
+                            exercises.options(
+                                    "Selecciona la respuesta correcta",
+                                    "La depresion es una enfermedad que se cura con medicamentos",
+                                    "La depresion aunque es una enfermedad no signifca que la persona tenga una debilidad",
+                                    "La depresion es equivalente a ser una persona suicida",
+                                    R.id.option2,
+                                    8, 7, false
+                            );
                             break;
                         case 8:
+                            exercises.write(
+                                    "Escribe el porcentaje de adultos que tienen depresion en Estados Unidos",
+                                    Arrays.asList("10%", "10", "10porciento", "diez", "diezporciento"),
+                                    9, 8, false
+                            );
                             break;
                         case 9:
+                            exercises.select(
+                                    "Selecciona la persona que posiblemente tenga depresion",
+                                    R.drawable.avatar1,
+                                    "Se siente mal desde hace unos dias por que reprobo su examen",
+                                    R.drawable.avatar2,
+                                    "No ha querido salir en las ultimas 2 semanas por la muerte de su mascota",
+                                    R.drawable.avatar3,
+                                    "Se ha sentido culpable, tiene apatia y se siente generalmente mal desde hace 1 mes",
+                                    R.drawable.avatar4,
+                                    "No ha comido en todo el dia por que los dias lluviosos le ponen triste",
+                                    3,
+                                    10, 9, false
+                            );
                             break;
                         case 10:
-                            break;
-                        case 11:
-                            break;
-                        case 12:
-                            break;
-                        case 13:
-                            break;
-                        case 14:
-                            break;
-                        case 15:
+                            exercises.select(
+                                    "Selecciona la respuesta correcta",
+                                    R.drawable.llorar,
+                                    "Llorar una vez es un indicio de depresion",
+                                    R.drawable.genetica,
+                                    "La depresion es 100% genetica",
+                                    R.drawable.hablar,
+                                    "Hablar sobre la depresion ayuda",
+                                    R.drawable.definitiva,
+                                    "La depresion ya tiene una cura definitiva",
+                                    1,
+                                    10, 10, true
+                            );
                             break;
                     }
                 });
             break;
+
             default:
                 exercises.setupFinishCourse();
                 break;
