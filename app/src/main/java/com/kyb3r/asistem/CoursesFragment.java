acd85084dd8ad79efa318cf9924146c3cde19d4a
Kyb3r Cipher <me@kyb3rcipher.com>
2023-11-26 10:45:46 -0600

    DepressionCourseActivity add string to file for translations

--- app/src/main/java/com/kyb3r/asistem/CoursesFragment.java
+++ app/src/main/java/com/kyb3r/asistem/CoursesFragment.java
@@ -57,7 +57,7 @@ public class CoursesFragment extends Fragment {
 
         elements = new ArrayList<>();
         elements.add(new CoursesList(getString(R.string.course1Title), getString(R.string.course1Description), R.drawable.banner_course_depression));
-        elements.add(new CoursesList(getString(R.string.course2Title), getString(R.string.course2Description), R.drawable.banner_course_fractures));
+        //elements.add(new CoursesList(getString(R.string.course2Title), getString(R.string.course2Description), R.drawable.banner_course_fractures));
 
         DatabaseHelper db = new DatabaseHelper(getContext());
         for (CoursesList course : elements) {
--- app/src/main/java/com/kyb3r/asistem/course/DepressionCourseActivity.java
+++ app/src/main/java/com/kyb3r/asistem/course/DepressionCourseActivity.java
@@ -34,116 +34,116 @@ public class DepressionCourseActivity extends AppCompatActivity {
                     switch (ExerciseClass.nextFragment) {
                         case 0:
                             exercises.options(
-                                    "Selecciona sintomas de la depresion:",
-                                    "Dolor de cabeza, Llanto, Cambio en la manera de vestirse",
-                                    "Apatía, Perdida de interés, Cambios en el apetito, Sentirse inultil/culpable",
-                                    "Estar solo todo el dia, dormir mucho, no jugar videojuegos",
+                                    getString(R.string.case0_instruction),
+                                    getString(R.string.case0_option1),
+                                    getString(R.string.case0_option2),
+                                    getString(R.string.case0_option3),
                                     R.id.option2,
                                     1, 0, false
                             );
                             break;
                         case 1:
                             exercises.write(
-                                    "Escribe un neurotransmisor que en menor medida genere la depresion",
-                                    Arrays.asList("serotonina", "dopamina", "norepinefrina"),
+                                    getString(R.string.case1_instruction),
+                                    Arrays.asList(getString(R.string.case1_word1), getString(R.string.case1_word2), getString(R.string.case1_word3)),
                                     2, 1, false
                             );
                             break;
                         case 2:
                             exercises.select(
-                                    "Selecciona un cambio que se produce en el cerebro de una persona con depresion",
+                                    getString(R.string.case2_instruction),
                                     R.drawable.pequeno,
-                                    "Lobulos/hipocampo mas pequeño",
+                                    getString(R.string.case2_text1),
                                     R.drawable.cerebro_blancoo,
-                                    "Cerebro vacio/blanco",
+                                    getString(R.string.case2_text2),
                                     R.drawable.visionrayosx,
-                                    "Vision rayos X",
+                                    getString(R.string.case2_text3),
                                     R.drawable.grande,
-                                    "Lobulos frontales e hipocampo mas grandes",
+                                    getString(R.string.case2_text3),
                                     1,
                                     3, 2, false
                             );
                             break;
                         case 3:
                             exercises.options(
-                                    "Cuanto tarda aproximadamente una persona con una enfermedad mental/depresion en pedir ayuda:",
-                                    "12 meses",
-                                    "6 meses",
-                                    "10 años",
+                                    getString(R.string.case3_instruction),
+                                    getString(R.string.case3_option1),
+                                    getString(R.string.case3_option2),
+                                    getString(R.string.case3_option3),
                                     R.id.option3,
                                     4, 3, false
                             );
                             break;
                         case 4:
                             exercises.options(
-                                    "¿Qué diferencia hay entre la depresión clínica y simplemente sentirse deprimido?",
-                                    "La depresión clínica es una respuesta temporal a situaciones como obtener malas calificaciones. En contraste, sentirse deprimido es una respuesta y afeccion medica que puede permsistir durante semanas",
-                                    "La depresión clínica es una afección médica que persiste durante al menos dos semanas y afecta significativamente la capacidad de trabajar, jugar o amar. En contraste, sentirse deprimido puede ser una respuesta temporal a situaciones como obtener malas calificaciones",
-                                    "La depresión clínica es una enfermedad que puede ser detectada por rayos X",
+                                    getString(R.string.case4_instruction),
+                                    getString(R.string.case4_option1),
+                                    getString(R.string.case4_option2),
+                                    getString(R.string.case4_option3),
                                     R.id.option2,
                                     5, 4, false
                             );
                             break;
                         case 5:
                             exercises.options(
-                                    "¿Cuáles son algunos tratamientos efectivos para la depresión?",
-                                    "Terapia, Medicamentos, Tratamientos experimentales como la estimulación magnética transcraneal.",
-                                    "Terapia, Comer mas, Salir con amigos",
-                                    "Terapia, Salir de viaje, Tratamientos experimentales como las drogas",
+                                    getString(R.string.case5_instruction),
+                                    getString(R.string.case5_text1),
+                                    getString(R.string.case5_text2),
+                                    getString(R.string.case5_text3),
                                     R.id.option1,
                                     6, 5, false
                             );
                             break;
                         case 6:
                             exercises.write(
-                                    "Escribe ¿Cómo se describe la terapia electroconvulsiva y en qué casos se menciona que puede ser especialmente útil como tratamiento para la depresión?",
-                                    Arrays.asList("terapia electroconvulsiva", "electroconvulsiva terapia", "electroconvulsiva"),
+                                    getString(R.string.case6_instruction),
+                                    Arrays.asList(getString(R.string.case6_word1), getString(R.string.case6_word2), getString(R.string.case6_word3)),
                                     7, 6, false
                             );
                             break;
                         case 7:
                             exercises.options(
-                                    "Selecciona la respuesta correcta",
-                                    "La depresion es una enfermedad que se cura con medicamentos",
-                                    "La depresion aunque es una enfermedad no signifca que la persona tenga una debilidad",
-                                    "La depresion es equivalente a ser una persona suicida",
+                                    getString(R.string.case7_instruction),
+                                    getString(R.string.case7_option1),
+                                    getString(R.string.case7_option2),
+                                    getString(R.string.case7_option3),
                                     R.id.option2,
                                     8, 7, false
                             );
                             break;
                         case 8:
                             exercises.write(
-                                    "Escribe el porcentaje de adultos que tienen depresion en Estados Unidos",
-                                    Arrays.asList("10%", "10", "10porciento", "diez", "diezporciento"),
+                                    getString(R.string.case8_instruction),
+                                    Arrays.asList(getString(R.string.case8_word1), getString(R.string.case8_word2), getString(R.string.case8_word3), getString(R.string.case8_word4), getString(R.string.case8_word5)),
                                     9, 8, false
                             );
                             break;
                         case 9:
                             exercises.select(
-                                    "Selecciona la persona que posiblemente tenga depresion",
+                                    getString(R.string.case9_instruction),
                                     R.drawable.avatar1,
-                                    "Se siente mal desde hace unos dias por que reprobo su examen",
+                                    getString(R.string.case9_text1),
                                     R.drawable.avatar2,
-                                    "No ha querido salir en las ultimas 2 semanas por la muerte de su mascota",
+                                    getString(R.string.case9_text2),
                                     R.drawable.avatar3,
-                                    "Se ha sentido culpable, tiene apatia y se siente generalmente mal desde hace 1 mes",
+                                    getString(R.string.case9_text3),
                                     R.drawable.avatar4,
-                                    "No ha comido en todo el dia por que los dias lluviosos le ponen triste",
+                                    getString(R.string.case9_text4),
                                     3,
                                     10, 9, false
                             );
                             break;
                         case 10:
                             exercises.select(
-                                    "Selecciona la respuesta correcta",
+                                    getString(R.string.case10_instruction),
                                     R.drawable.llorar,
-                                    "Llorar una vez es un indicio de depresion",
+                                    getString(R.string.case10_text1),
                                     R.drawable.genetica,
-                                    "La depresion es 100% genetica",
+                                    getString(R.string.case10_text2),
                                     R.drawable.hablar,
-                                    "Hablar sobre la depresion ayuda",
+                                    getString(R.string.case10_text3),
                                     R.drawable.definitiva,
-                                    "La depresion ya tiene una cura definitiva",
+                                    getString(R.string.case10_text4),
                                     1,
                                     10, 10, true
                             );
